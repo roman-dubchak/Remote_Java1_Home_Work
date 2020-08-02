c354e3d9714f1d50f554f9a844922b7d950d0575
Roman Dubchak <dubrathers@mail.ru>
2020-08-02 19:43:42 +0300

    add task 5-7*

--- lesson2/lesson2.java
+++ lesson2/lesson2.java
@@ -33,6 +33,9 @@ public class lesson2 {
             if (arr1_2[i] == 0) {
                 arr1_2[i] = 1;
             } else arr1_2[i] = 0;
+//            arr[i] = (arr[i] == 1)? 0 : 1;
+//            arr[i] ^= 1;
+//            arr[i] = arr[i] ^ 1;
         }
     }
 
@@ -41,6 +44,12 @@ public class lesson2 {
             arr2_2[i] = i * 3 + 1;
         }
     }
+
+//    private static void arrNum ( int[] a, int step, int shift){
+//        for (int i = 0; i < a.length; i++) {
+//            a[i] = i * step + shift; // всесто фикс значений 3 и 1
+//        }
+//    }
 
     private static void multiNumberArr (int[] arr3_1){
         for (int i = 0; i < arr3_1.length; i++) {
@@ -65,6 +74,81 @@ public class lesson2 {
             if (arr4_2[i] < minN) {
                 minN = arr4_2[i];
             }
+        }
+    }
+    private static void arrDiagonaly ( int[][] arr5){
+        for (int y = 0; y < arr5.length; y++) {
+            arr5[y][y] = 1;
+            arr5[y][arr5.length - 1 - y] = 1;
+        }
+    }
+
+    private static boolean chekBalace(int[] arr) {
+        int right = 0, left = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            left = left + arr[i];
+            for (int j = i + 1; j < arr.length; j++)
+                right = right + arr[j];
+            if (right == left) return true;
+            right = 0;
+        }
+        return false;
+    }
+
+    private static boolean chekBalace2(int[] arr) {
+        int sumArr = 0;// rigth = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sumArr = sumArr + arr[i];
+        }
+        return sumArr % 2 == 0;
+//        for (int j = 0; j < arr.length; j++) {
+//            rigth = rigth + arr[j];
+//            if (rigth == sumArr) return true;
+//        }
+//        return true;
+    }
+
+    private static void shifter(int[] arr, int value) {
+        boolean dis;
+        if (value < 0) {
+            dis = true;
+            value = -value;
+        } else dis = false;
+
+        value %= arr.length;
+        int lastInd = arr.length - 1;
+
+        for (int i = 0; i < value; i++) {
+            int temp = (dis) ? arr[0] : arr[lastInd];
+            for (int j = 0; j < lastInd; j++) {
+                if (dis)
+                    arr[j] = arr[j + 1];
+                else
+                    arr[lastInd - j] = arr[lastInd - j - 1];
+            }
+            if (dis)
+                arr[lastInd] = temp;
+            else
+                arr[0] = temp;
+        }
+    }
+
+    private static void shifter2(int[] arr, int value) {
+        boolean dis = (value < 0);
+        if (dis) value = -value;
+
+        value %= arr.length;
+        if (value == 0) return;
+        int shifter = (dis) ? arr.length - value : value;
+
+        int lastInd = arr.length - 1;
+        for (int i = 0; i < shifter; i++) {
+            int temp = arr[arr.length - 1];
+            for (int j = 0; j < lastInd; j++) {
+                arr[lastInd - j] = arr[lastInd - j - 1];
+//           System.arraycopy(arr, 0, arr, 1, arr.length - 1); // вместо второго for
+            }
+            arr[0] = temp;
         }
     }
 }
