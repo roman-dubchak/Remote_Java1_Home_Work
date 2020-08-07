ee25dbb86c7a510f02d68b501c524c627a1ca4b4
Roman Dubchak <dubrathers@mail.ru>
2020-08-07 10:00:53 +0300

    add HW Lesson 4, task 1-7

--- leeson4_emloyee/Employee.java
+++ leeson4_emloyee/Employee.java
@@ -4,22 +4,58 @@ public class Employee {
     private String fio;
     private float salary;
     private int age;
+    private int id;
 
-    Employee(){
-        this.fio = "Dubchak Roman Anatolievich";
-        this.salary = 150123.46f;
-        this.age = 33;
+
+    Employee(String fio, float salary, int age){
+        this.fio = fio;
+        this.salary = salary;
+        this.age = age;
     }
 
-    public String setFio() {
+    public String getFio() {
         return this.fio;
     }
 
     public float getSalary() {
-        return salary;
+        return this.salary;
     }
 
-    public int setAge() {
+    public int getAge() {
         return this.age;
+    }
+
+    private float getUpSalary(){
+        return this.salary += 5000.00f; // можно содать сеттер с заданым дополнительным полем "Премия" = 5000.00f
+    }
+
+    void salaryUp (Employee[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].getAge() > 45) {
+                arr[i].getUpSalary();
+                System.out.println("For employee: " + arr[i].getFio() + " " + arr[i].getAge() + " years" // for check
+                        + " the new salary: " + arr[i].getSalary() + "!");
+            }
+        }
+    }
+
+    float averageSalary (Employee[] arr){
+        float sumSalary = 0.0f, averageS = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sumSalary = sumSalary + arr[i].getSalary();
+        }
+        averageS = sumSalary / arr.length;
+        System.out.println("The average salary of all employees is " + averageS);
+        return averageS;
+    }
+
+    float averageAge (Employee[] arr){
+        float sumAge = 0.0f, averageA = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sumAge = sumAge + arr[i].getAge();
+        }
+        averageA = sumAge / arr.length;
+        System.out.println("The average age of all employees is " + averageA);
+        return averageA;
     }
 }
--- leeson4_emloyee/main.java
+++ leeson4_emloyee/main.java
@@ -2,11 +2,31 @@ package repo_java1_Home_Work.leeson4_emloyee;
 
 public class main {
      public static void main(String[] args) {
-         Employee employee = new Employee();
-         employee.setAge();
-         employee.setFio();
-//         employee.getSalary() = 444.44f;
-         System.out.println("Employe: " + employee.setFio() + ";"
-                 + " age: " + employee.setAge() + ".");
-    }
+         Employee employee = new Employee("Roman  D.A.", 1200.00f, 33);
+         employee.getFio();
+         employee.getAge();
+         System.out.println("Employee: " + employee.getFio() + ";"
+                 + " age: " + employee.getAge() + ".");
+         System.out.println();
+
+         Employee[] staff = new Employee[5];
+         staff[0] = employee;
+         staff[1] = new Employee("Petr A.N.", 1200.00f, 45);
+         staff[2] = new Employee("Arkagy S.C.", 1200.00f, 48);
+         staff[3] = new Employee("Dmytriy A.R.", 1200.00f, 39);
+         staff[4] = new Employee("Artem P.V.", 1200.00f, 32);
+
+         System.out.println("Employees over 40 years of age: ");
+         for (int i = 0; i < staff.length; i++) {
+             if (staff[i].getAge() > 40)
+                 System.out.println("Employee: "+ staff[i].getFio() +
+                         ";" + " age: " + staff[i].getAge() +
+                         ";" + " salary: " + staff[i].getSalary());
+         }
+         System.out.println();
+
+         employee.salaryUp(staff);
+         employee.averageSalary(staff);
+         employee.averageAge(staff);
+     }
 }
